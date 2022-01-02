ca04bc4e0beb2a33ff006a74a277a93287ac768e
TikvaYosef <tikvayosef13@gmail.com>
2022-01-02 11:35:09 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSQL_LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Shop> list1 = new List<Shop>();
+            try
+            {
+                string conectionS = "Data Source=DESKTOP-5E70RM2;Initial Catalog=msdb;Integrated Security=True";
+                using (SqlConnection connection = new SqlConnection(conectionS))
+                {
+                    connection.Open();
+                    string query = @"SELECT * FROM Shop";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    SqlDataReader datafromdb = command.ExecuteReader();
+
+                    if (datafromdb.HasRows)
+                    {
+                        while (datafromdb.Read())
+                        {
+                            list1.Add(new Shop(datafromdb.GetString(0),datafromdb.GetInt(1));
+                            //Console.WriteLine(datafromdb.GetString(1));
+
+                        }
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("no rows in table");
+                    }
+                    connection.Close();
+                }
+               
+            }
+            catch(SqlException)
+            {
+                Console.WriteLine("error");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+        
+    }
+    
+}
---
+++ Shop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSQL_LINQ
+{
+    internal class Shop
+    {
+       public string name;
+        public int age;
+
+        public Shop(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+    }
+}
